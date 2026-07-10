65931f0204ae1c68f619b78e299ec53e9e27f76c
niranth <niran.thonphudsa@gmail.com>
2026-07-10 16:52:46 +0700

    test: detect first

---
+++ train_model/train.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLO model (you can choose n, s, m, l, or x versions)
+model = YOLO("yolo26n.pt")
+
+# Start training on your custom dataset
+model.train(data="path/to/dataset.yaml",
+             epochs=100,
+             imgsz=640
+            )
