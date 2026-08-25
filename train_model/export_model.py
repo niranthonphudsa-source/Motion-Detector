c790a7ff865a0e8fd2bdb7a497ec7f8e3e31f089
niranth <niran.thonphudsa@gmail.com>
2026-08-25 13:17:39 +0700

    test openvino

--- train_model/export_model.py
+++ train_model/export_model.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO() 
+model = YOLO("main\yolo26n-pose.pt") 
 model.export(format='openvino')
 # เรียกใช้โมเดล OpenVINO
 ov_model = YOLO('yolo26n-pose_openvino_model/')
